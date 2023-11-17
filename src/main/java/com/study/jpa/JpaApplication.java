3b85405ac5efe356294c3742d1a1741d8450a11d
jeonghyun <chlwjdgus822@gmail.com>
2023-11-17 16:52:56 +0900

    initial commit

---
+++ src/main/java/com/study/jpa/JpaApplication.java
@@ -0,0 +1,14 @@
+package com.study.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	// 이게 내장 톰캣 서버를 가동 시켜주는 주문임
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApplication.class, args);
+	}
+
+}
