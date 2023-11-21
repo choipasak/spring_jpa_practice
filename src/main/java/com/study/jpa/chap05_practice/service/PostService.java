3c5b04ad000172d24f2bb51d9d067146a8b657a0
jeonghyun <chlwjdgus822@gmail.com>
2023-11-21 16:40:01 +0900

    #7. 종합 연습예제1

---
+++ src/main/java/com/study/jpa/chap05_practice/service/PostService.java
@@ -0,0 +1,18 @@
+package com.study.jpa.chap05_practice.service;
+
+import com.study.jpa.chap05_practice.repository.HashTagRepository;
+import com.study.jpa.chap05_practice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional // JPA 레파지토리는 트랜잭션 단위로 동작하기 때문에 반드시 작성 해 줘야 한다@
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final HashTagRepository hashTagRepository;
+}
